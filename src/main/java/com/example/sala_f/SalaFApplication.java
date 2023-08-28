fac498c6129d2c673a823f8e0b538132ba8532d4
helena costa <99986997+helenabc01@users.noreply.github.com>
2023-08-28 22:50:04 +0000

    commit

--- src/main/java/com/example/sala_f/SalaFApplication.java
+++ src/main/java/com/example/sala_f/SalaFApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class SalaFApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalaFApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Germinare Tech - Lena";
 	}
 
 }
