d41a449e180687697270622626e17c418050b6dd
Helena <helena.costa@germinare.org.br>
2023-08-28 18:58:04 -0300

    first commit

---
+++ src/main/java/com/example/sala_f/SalaFApplication.java
@@ -0,0 +1,13 @@
+package com.example.sala_f;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaFApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sala_f/SalaFApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sala_f;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaFApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
